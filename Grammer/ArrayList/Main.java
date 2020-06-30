e432a0f4414283d3b003fe60a1bf605017717999
theheroproject <thenaheroproject@gmail.com>
2020-06-30 22:22:51 +0900

    good evening

---
+++ Grammer/ArrayList/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+        ArrayList<Integer> numList = new ArrayList<>();
+
+        numList.add(1);
+        numList.add(2);
+        numList.add(3);
+        numList.add(4);
+        numList.add(5);
+        for (int num : numList){
+            System.out.println(num * num);
+        }
+    }
+}
